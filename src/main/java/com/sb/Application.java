3d6628592b3f3cf0f7509d1cf5939abd8de2b5f5
= <=>
2020-06-08 13:35:05 +0530

    HIS-5: fix is provided

--- src/main/java/com/sb/Application.java
+++ src/main/java/com/sb/Application.java
@@ -8,10 +8,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Application {
 
 	public static void main(String[] args) {
-	ConfigurableApplicationContext ctx =	SpringApplication.run(Application.class, args);
-	ctx.close();
+		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+		ctx.close();
 		SpringApplication.run(Application.class, args);
 		System.out.println("SpringbootApp...");
+		System.out.println(ctx.getClass());
 	}
 
 }
